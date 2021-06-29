685d81b4de33cc8fa5a8338829adb3ad2f01bb0f
RianneStreef <riannestreef@gmail.com>
2021-06-29 18:13:51 +0200

    insta feed

--- src/Components/InstaFeed/InstaFeed.jsx
+++ src/Components/InstaFeed/InstaFeed.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./InstaFeed.css";
+
 import { content } from "../../content/languages";
 
 const InstaFeed = (props) => {
@@ -9,9 +11,11 @@ const InstaFeed = (props) => {
     ? (language = content.english)
     : (language = content.french);
   return (
-    <div id="insta">
+    <div id="insta" className="insta">
       <h2>{language.instaFeed}</h2>
       {/* <div className="elfsight-app-6170eb17-0cb7-4059-8fbe-24a99c7b7692"></div> */}
+
+      <div data-mc-src="09ca44f8-ca9d-46f8-b4c5-a05206f21743#instagram"></div>
     </div>
   );
 };
